3b1e1cd30717e6d3b600be899cc8e90440df20cc
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-05-19 20:18:32 +0800

    Add initial sockets code

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ var express = require("express");
 var app = express();
 var settings = require("./controllers/settings.js");
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 require("./controllers/config.js")(app, express);
 app.use(require("./controllers/routes.js"));
 
-app.listen(settings.PORT);
+const connectedIds = [];
+io.on('connection', socket => {
+    connectedIds.push(socket.id);
+    socket.on('disconnect', () => connectedIds.splice(connectedIds.findIndex(socket.id), 1));
+    socket.on('question', data => {
+        // Store the question data into the database of existing questions
+    });
+});
+
+server.listen(settings.PORT);
 console.info("Listening on port " + settings.PORT + " in " + app.get("env") + " mode.");
